5517b6a877d8bd912a2c8f7b687c25b688bc31de
Rodrigo Junqueira <rodrigo.fpjunqueira@gmail.com>
2020-08-28 17:49:32 -0300

    Organized code into classes.

---
+++ Gif.js
@@ -0,0 +1,41 @@
+class Gif {
+
+    constructor(id,x,y,timer) {
+        this.id = id;
+        this.position = createVector(x,y);
+        this.file = ceil(random(4));
+        this.timer = timer;
+        this.zero = this.timer.getTime();
+        this.loadingSize = 20;
+        this.loading = true;
+        this.gif = createImg('assets/' + this.file + '.gif', 'Gifs by AXEL_DE_STAMPA', '', callback(this.id));  
+        console.log('Loading ' + this.id + '...');
+        this.gif.hide();
+    }
+
+    show() {
+        if (this.loading) {
+            push();
+            noFill();
+            rectMode(CENTER);
+            let loading = this.loadingSize*sin((this.zero+timer.getTime())*0.1);
+            rect(this.position.x,this.position.y,loading,loading);
+            pop();
+        } 
+    }
+
+    loaded() {
+        this.gif.size(100,AUTO);    
+        this.gif.position(this.position.x,this.position.y);
+        this.gif.show();
+        this.loading = false;
+    }
+
+    kill() {
+        this.gif.remove();
+    }
+
+
+
+
+}
